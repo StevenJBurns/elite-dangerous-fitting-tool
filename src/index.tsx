28cd49ec76fd7ae173d131483aceeefa2d67ac8f
Steven J Burns <stevenjburns@msn.com>
2020-12-09 14:34:18 -0700

    chore: rename js files to tsx for TS

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('react-root')
 );
 
