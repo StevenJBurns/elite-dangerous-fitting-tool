c3cfa2ba1fbd6d3f3c6f59214aa649cfea67e330
StevenJBurns <stevenjburns@msn.com>
2021-01-07 13:50:55 -0700

    feat: implement React Router at App level

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('react-root')
 );
 
