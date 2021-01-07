12986c7bf5e3f850828969759e2cd1f6e31853cc
StevenJBurns <stevenjburns@msn.com>
2021-01-07 13:51:40 -0700

    feat: routing and MUI implementation at the App and default page level

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import { Home } from 'components/pages/Home/Home';
+import { Ships } from 'components/pages/Ships/Ships';
+import { Modules } from 'components/pages/Modules/Modules';
+import { Fittings } from 'components/pages/Fittings/Fittings';
+import { About } from 'components/pages/About/About';
 import './App.css';
 
 const ShipList = React.lazy(() => import('./ShipList'));
@@ -8,15 +18,38 @@ const App = () => {
 
   return (
     <>
-      <header></header>
+      <AppBar position="relative">
+        <Container>
+          <Typography variant="h4">
+            ED FITTING TOOL
+          </Typography>
+        </Container>
+      </AppBar>
       <main>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/ships" exact>
+            <Ships />
+          </Route>
+          <Route path="/modules" exact>
+            <Modules />
+          </Route>
+          <Route path="/fittings" exact>
+            <Fittings />
+          </Route>
+          <Route path="/about" exact>
+            <About />
+          </Route>
+        </Switch>
         <React.Suspense fallback={<h1> *** Loading ***</h1>}>
           <ShipList />
         </React.Suspense>
       </main>
-      <footer>
-        <h4>&copy; {currentYear} &bull; SJB</h4>
-      </footer>
+      <BottomNavigation component="footer">
+        <Typography variant="h6">&copy; {currentYear} &bull; SJB</Typography>
+      </BottomNavigation>
     </>
   )
 };
--- src/ShipList.tsx
+++ src/ShipList.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
 import Select from '@material-ui/core/Select';
 import MenuItem from "@material-ui/core/MenuItem";
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-// import TableContainer from '@material-ui/core/TableContainer';
+import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Divider from '@material-ui/core/Divider';
 import { Ship, ShipSize } from 'types/Ship';
 import ShipData from 'data/ships.json';
 
+const useStyles = makeStyles(() => ({
+  table: {
+    maxWidth: '800px',
+    margin: '1rem auto',
+  },
+  tableHeader: {
+    backgroundColor: 'gainsboro',
+  }
+}));
+
 const ShipList = () => {
+  const classes = useStyles();
   const [shipData, setShipData] = React.useState<Ship[]>(ShipData);
   const [shipSizeFilter, setShipSizeFilter] = React.useState<ShipSize | "All">("All");
 
@@ -24,6 +36,7 @@ const ShipList = () => {
   return (
     <>
       <Select
+        style={{ width: '8rem'}}
         defaultValue={"All"}
         value={shipSizeFilter}
         onChange={(e: React.ChangeEvent<{ value: any }>) => setShipSizeFilter(e.target.value)}
@@ -34,24 +47,30 @@ const ShipList = () => {
         <MenuItem value={ShipSize.Large}>Large</MenuItem>
       </Select>
       <Divider />
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell>Manufacturer</TableCell>
-            <TableCell>Size</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          { shipData.map(ship =>
-            <TableRow key={ship.name}>
-              <TableCell>{ship.name}</TableCell>
-              <TableCell>{ship.manufacturer}</TableCell>
-              <TableCell>{ship.size}</TableCell>
-            </TableRow>)
-          }
-        </TableBody>
-      </Table>
+      <TableContainer>
+        <Table className={classes.table} size="small">
+          <TableHead className={classes.tableHeader}>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Manufacturer</TableCell>
+              <TableCell>Base Cost</TableCell>
+              <TableCell>Size</TableCell>
+              <TableCell>Crew</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            { shipData.map(ship =>
+              <TableRow key={ship.name}>
+                <TableCell width={'160px'}>{ship.name}</TableCell>
+                <TableCell width={'120px'}>{ship.manufacturer}</TableCell>
+                <TableCell width={'96px'}>{ship.baseCost}</TableCell>
+                <TableCell>{ship.size}</TableCell>
+                <TableCell width={'120px'}>{ship.crew}</TableCell>
+              </TableRow>)
+            }
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
